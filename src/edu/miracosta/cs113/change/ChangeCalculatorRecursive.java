2936c3dd018aaa655d05cdc9ed3ff5496cee6790
kc-cat11 <kc.cat888@gmail.com>
2025-03-11 20:03:45 -0700

    Finished but with error where the checker cannot find my combinations. It is generating the correct combinations, the checker just cannot find them for mysterious reasons.

--- src/edu/miracosta/cs113/change/ChangeCalculatorRecursive.java
+++ src/edu/miracosta/cs113/change/ChangeCalculatorRecursive.java
@@ -26,7 +26,46 @@ public class ChangeCalculatorRecursive extends ChangeCalculator {
         // TODO:
         // Implement a recursive solution following the given documentation.
         List<String> combinations = new ArrayList<>();
-
+        checkChangeCombinations(combinations, cents, 0, 0, 0, 0);
         return combinations;
     }
+
+    public static void checkChangeCombinations(List<String> list, int cents, int pennies, int nickels, int dimes, int quarters)
+    {
+       String currentChange = "";
+       /*
+        int penniesNum = pennies;
+        int nickelsNum = nickels;
+        int dimesNum = dimes;
+        int quartersNum = quarters;
+        int change = cents;
+
+        */
+        if(cents >= 25)
+        {
+            checkChangeCombinations(list, cents - 25, pennies, nickels, dimes, quarters + 1);
+        }
+        if(cents >= 10)
+        {
+            checkChangeCombinations(list, cents - 10, pennies, nickels, dimes + 1, quarters);
+        }
+        if(cents >= 5)
+        {
+            checkChangeCombinations(list, cents - 5, pennies, nickels + 1, dimes, quarters);
+        }
+        if(cents >= 1) {
+            checkChangeCombinations(list, cents - 1, pennies + 1, nickels, dimes, quarters);
+        }
+        if(cents == 0) {
+            currentChange = quarters + "q " + dimes + "d " + nickels + "n " + pennies + "p";
+            if(list.contains(currentChange))
+            {
+                return;
+            }
+            else {
+                list.add(currentChange);
+            }
+        }
+    }
+
 }
